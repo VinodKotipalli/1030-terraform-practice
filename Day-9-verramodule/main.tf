deae75d6886c903cf3a461523a8cef0e5f499933
Kotipalli Saivinod <saivinodkotipalli2003@zohomail.in>
2026-07-18 16:18:53 +0530

    all

---
+++ Day-9-verramodule/main.tf
@@ -0,0 +1,7 @@
+module "Veera" {
+ source =  "github.com/CloudTechDevOps/1030-terraform-practice/Day-9-Modules-ec2"
+ ami_id = "ami-01edba92f9036f76e"
+ instance_type = "t3.micro"
+ tags =  "veera-instance"
+}
+
